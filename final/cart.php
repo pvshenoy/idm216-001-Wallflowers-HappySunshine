077110d8ef409759ddab0c892e0e807970091d94
cjk327 <cjk327@drexel.edu>
2023-03-14 22:49:49 -0400

    cart issue

--- final/cart.php
+++ final/cart.php
@@ -3,6 +3,9 @@ include_once 'app.php';
 
 $site_url = site_url();
 
+
+// ($user['isGuest']==0) ? "success": redirect_to('/auth/login.php') ;
+
 $orderTotal = 0;
 $order = get_order_by_user_id($user['id']);
 
@@ -13,8 +16,8 @@ if ($order->num_rows > 0) {
     } // End while loop
     echo '</ul>';
     echo "$" . $orderTotalPrice;
-    echo "<a href='";
 
+    echo "<a href='";
     if ($user['isGuest']) {
         echo "{$site_url}/login.php";
     }
@@ -22,11 +25,11 @@ if ($order->num_rows > 0) {
         echo "{$site_url}/checkout.php";
     }
     echo "'>Checkout</a>";
-    
 } 
 else {
-    echo 'cart is empty, start an order!';
-    echo "<a href='{$site_url}/main.php'>Menu</a>";
+    echo 'empty cart';
+    echo "<a href='{$site_url}/main.php'>menu</a>";
 }
+
 
 ?>
